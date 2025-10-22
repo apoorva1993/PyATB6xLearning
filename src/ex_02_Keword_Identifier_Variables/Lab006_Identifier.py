b8f101b760b29ee3481eee0d639a095af2f53f26
apoorva1993 <baranwal.apoorva@gmail.com>
2025-10-22 23:03:33 +0530

    Update the project

---
+++ src/ex_02_Keword_Identifier_Variables/Lab006_Identifier.py
@@ -0,0 +1,15 @@
+# 1- Variable Name
+# 2- Rules for this:
+#     a- _ (underscore) can be a variable
+#     b- Do not have a number as the first thing in a variable name
+#     c- AlphaNumeric is possible as a variable name or identifier
+age = 65
+_age = 65
+print(age)
+print(_age)
+
+
+_ = 12
+print(_)
+_ = _ + 1
+print(_)
