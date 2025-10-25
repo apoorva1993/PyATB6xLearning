864713733e7a6acc33c0e2434f8cf6deef69becd
apoorva1993 <baranwal.apoorva@gmail.com>
2025-10-25 13:55:45 +0530

    Update the project

---
+++ src/ex_02_Keword_Identifier_Variables/Lab012_Multiple_Variables.py
@@ -0,0 +1,8 @@
+# can assign the multiple variables in Python
+a, b, c = 45, 54, 67
+print("value of a, b, c are", a, b, c)
+
+a, b, c = 45, 5.4, "Apoorva"
+print("value of a, b, c are", a, b, c)
+print("value of a, b, c are", a, b, c, sep=" | ")
+print(a, b, c, sep=" | ")
---
+++ src/ex_02_Keword_Identifier_Variables/Lab014_Math_Functions.py
@@ -0,0 +1,7 @@
+# To find the max - function - prebuilt functions
+# Max number between 3,4
+result = max(3, 4, 100, 13, 201)
+print("max number is", result)
+
+result = min(3, 4, 100, 13, 201)
+print("min number is", result)
