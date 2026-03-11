e4523da2b121f73ee4a2826108b6e9e79b071580
apoorva1993 <baranwal.apoorva@gmail.com>
2026-03-11 10:35:12 +0530

    add the files in src folder

---
+++ src/ex_03_Literals_Variables/Lab017_Multi_Comment.py
@@ -0,0 +1,11 @@
+age = 32  # int
+
+"""
+This is a multi Comment
+1- Author: Apoorva
+2- Information
+"""
+
+# This is single line comment
+
+print(age)
---
+++ src/ex_03_Literals_Variables/Lab018_Multi_Comments.py
@@ -0,0 +1,8 @@
+age = 32
+"""
+This is a multi Comment
+1- Author: Apoorva
+2- Information: Print the age of Apoorva
+"""
+
+print("Age of Apoorva: ", age)
